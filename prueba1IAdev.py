69b2ef2768a4d0320eb977275e71633b5bf7ad6b
Johan Salazar Prada <johansalazarprada@gmail.com>
2025-05-15 21:40:17 -0500

    Update prueba1IAdev.py

--- prueba1IAdev.py
+++ prueba1IAdev.py
@@ -21,7 +21,7 @@ st.markdown(
     <style>
         [data-testid="stAppViewContainer"] {
             background-image: url("https://thumbs.dreamstime.com/b/green-energy-planet-earth-solar-panels-windmills-nature-areas-276574908.jpg");
-            background-size: 100% ; /* Ajusta el tamaño según necesites */
+            background-size: 50% ; /* Ajusta el tamaño según necesites */
             background-position: center;
         }
     </style>
