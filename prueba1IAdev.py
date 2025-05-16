34292c987f34eda81ed3876990654b037fc99335
Johan Salazar Prada <johansalazarprada@gmail.com>
2025-05-16 01:01:19 -0500

    Update prueba1IAdev.py

--- prueba1IAdev.py
+++ prueba1IAdev.py
@@ -566,7 +566,7 @@ with tab3:
                     st.pyplot(plt)
         
                 # Filtrar datos y guardar archivo final
-                df_filtrado = df.iloc[:5088]
+                #df_filtrado = df.iloc[:5088]
                 df_filtrado.to_csv('BaseDatos_filtrado.csv', index=False)
                 st.write("✅ ¡Archivo BaseDatos_filtrado.csv guardado con éxito!")
         
