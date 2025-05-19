857c8a78d081be9d9555b8869fec3b40a8e4f2ba
Johan Salazar Prada <johansalazarprada@gmail.com>
2025-05-18 22:13:10 -0500

    Update prueba1IAdev.py

--- prueba1IAdev.py
+++ prueba1IAdev.py
@@ -23,7 +23,7 @@ st.markdown(
     """
     <style>
         [data-testid="stAppViewContainer"] {
-            background-image: url("https://github.com/SevastianPrada/sturdy-meme/blob/e8b703eef1ba48789c9c7182251fc70b33e185bc/SkyGhil.avif");
+            background-image: url("https://github.com/SevastianPrada/sturdy-meme/blob/main/Asd.jpg");
             background-size: 100% ; /* Ajusta el tamaño según necesites */
             background-position: center;
         }
